1d17ef71724db65b9361068757ff9b5f95a1de17
Thakur <anju.thakur@accenture.com>
2017-12-11 13:36:10 -0600

    adding transaction management code for all methods

--- src/main/java/org/superbiz/moviefun/HomeController.java
+++ src/main/java/org/superbiz/moviefun/HomeController.java
@@ -44,32 +44,26 @@ public class HomeController {
 
     public String setup(Map<String, Object> model) {
 
-        TransactionStatus transactionStatus= platformTransactionManager.getTransaction(null);
+        /*TransactionStatus transactionStatus= platformTransactionManager.getTransaction(null);
 
         System.out.println("#####################################    Transaction status " + transactionStatus);
-
-        moviesBean.addMovie(new Movie("Wedding Crashers", "David Dobkin", "Comedy", 7, 2005));
+*/
+      /*  moviesBean.addMovie(new Movie("Wedding Crashers", "David Dobkin", "Comedy", 7, 2005));
         moviesBean.addMovie(new Movie("Starsky & Hutch", "Todd Phillips", "Action", 6, 2004));
         moviesBean.addMovie(new Movie("Shanghai Knights", "David Dobkin", "Action", 6, 2003));
         moviesBean.addMovie(new Movie("I-Spy", "Betty Thomas", "Adventure", 5, 2002));
         moviesBean.addMovie(new Movie("The Royal Tenenbaums", "Wes Anderson", "Comedy", 8, 2001));
         moviesBean.addMovie(new Movie("Zoolander", "Ben Stiller", "Comedy", 6, 2001));
-        moviesBean.addMovie(new Movie("Shanghai Noon", "Tom Dey", "Comedy", 7, 2000));
+        moviesBean.addMovie(new Movie("Shanghai Noon", "Tom Dey", "Comedy", 7, 2000));*/
+  /*      platformTransactionManager.commit(transactionStatus);*/
 
-        platformTransactionManager.commit(transactionStatus);
-
+        createMovies();
+        createAlbums();
         model.put("movies", moviesBean.getMovies());
-
-       /* TransactionStatus transactionStatusAlb = platformTransactionManagerAlbums.getTransaction(null);
-        albumsBean.addAlbum(new Album("artist","title",2017,10));
-
-        System.out.println("#####################################    Transaction status " + transactionStatus.isCompleted());
-        System.out.println("#####################################    Transaction status " + transactionStatus.isNewTransaction());
-        platformTransactionManagerAlbums.commit(transactionStatusAlb);
-        System.out.println("#####################################    Transaction status " + transactionStatus.isCompleted());
-*/
-
         model.put("albums", albumsBean.getAlbums());
+
+
+
 
         return "setup";
     }
@@ -83,10 +77,10 @@ public class HomeController {
         platformTransactionManager.commit(transactionStatus);
     }
     public void createAlbums(){
-        TransactionStatus transactionStatus= platformTransactionManager.getTransaction(null);
+        TransactionStatus transactionStatus= platformTransactionManagerAlbums.getTransaction(null);
         for (Album album : albumFixtures.load()) {
             albumsBean.addAlbum(album);
         }
-        platformTransactionManager.commit(transactionStatus);
+        platformTransactionManagerAlbums.commit(transactionStatus);
     }
 }
